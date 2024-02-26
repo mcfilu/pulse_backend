d6b011178030659e6f6bf4d902bb567fd0765709
mcfilu <filip.1olszewski@gmail.com>
2024-02-26 13:35:03 +0000

    updating the gitognore file to use the official blueprint for python based projects plus update of the compose.yaml to mount the volume

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,11 +1,21 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 import requests
 
 router = APIRouter()
 
+#blueprint for pydantic basemodel class that represents the outputed data
+class UserData(BaseModel):
+    id: int
+    email: str
+    first_name: str
+    last_name: str
+    avatar: str
 
+#
 @router.get("/users/{user_id}", tags=["users"])
-async def get_user(user_id: int):
+async def get_user(user_id: int) -> UserData:
     response = requests.get(f"https://reqres.in/api/users/{user_id}")
-    return response.json()
+    user_instance = UserData(**response.json()['data'])
+    return user_instance
     # return {"Message": "hello"}
