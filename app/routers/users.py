3a397996822987f9cc39ad7d15dc239fe6f350b3
mcfilu <filip.1olszewski@gmail.com>
2024-02-26 13:42:45 +0000

    applying the error handling, plus pydantic class and data formatting before return

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 import requests
 
@@ -12,10 +12,17 @@ class UserData(BaseModel):
     last_name: str
     avatar: str
 
-#
+"""
+api endpoiint at /users/<int> that loads the user info from regres.in external api, 
+takes the information from ['data'], ommits the unecessary data, puts that into the pydantic object
+and return to client
+"""
 @router.get("/users/{user_id}", tags=["users"])
 async def get_user(user_id: int) -> UserData:
     response = requests.get(f"https://reqres.in/api/users/{user_id}")
+    if response.status_code != 200:
+        raise HTTPException(status_code = response_status_code, detail=f"Error accessing regres.in Api at /users/{user_id} ")
+
     user_instance = UserData(**response.json()['data'])
     return user_instance
     # return {"Message": "hello"}
