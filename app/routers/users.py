8ad3d5d86dec4723546cc8024e27aa0db49b21d4
mcfilu <filip.1olszewski@gmail.com>
2024-02-27 18:05:15 +0000

    making the get_user function docstring more detailed

--- app/routers/users.py
+++ app/routers/users.py
@@ -9,9 +9,11 @@ router = APIRouter()
 @router.get("/users/{user_id}", tags=["users"])
 async def get_user(user_id: int) -> UserData:
     """
-    api endpoiint at /users/<int> that loads the user info from regres.in external api, 
+    Api endpoint at /users/id that loads the user info from the int argument passed to the url and utilises HTTP request to the regres.in external api, 
     takes the information from ['data'], ommits the unecessary data, puts that into the pydantic object
-    and return to client
+    and returns it to the client.\n\n
+    Raises:
+        HTTPException: If regres.in api returns any other code than 200, the HTTPException is raised, the corresponding HTTP status code passed and the error message displayed.
     """
     async with httpx.AsyncClient() as client:
         response = await client.get(f"https://reqres.in/api/users/{user_id}")
